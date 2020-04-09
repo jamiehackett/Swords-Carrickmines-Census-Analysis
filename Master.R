a02574984a34e3cf112fba51be944a3b62243607
Jamie Hackett <jami3hack3tt@gmail.com>
2020-04-09 01:35:45 +0100

    First area of investigation which is of age distribution between the two areas. Oh and the areas are now Baldoyle and Sandyford.

--- Master.R
+++ Master.R
@@ -6,6 +6,9 @@
 library(data.table)
 library(stringr)
 library(dplyr)
+library(ggplot2)
+library(tidyverse)
+library(tibble)
 
 ######## Data inject and cleanup ########
 
@@ -19,41 +22,68 @@ areas <- read.csv("Areas.csv")
 #Joining the data in a new dataframe
 merged <- merge(census, areas, by=c("GUID"))
 
-#Creating a new dataframe that just focuses on Swords and Carrickmines.
-data_swords <- merged %>% filter(str_detect(EDNAME, "Swords"))
+#Creating a new dataframe that just focuses on Baldoyle and Sandford
+data_baldoyle <- merged %>% filter(str_detect(EDNAME, "Baldoyle"))
 #Drop the first three columns
-data_swords <- data_swords[ -c(1:3)]
+data_baldoyle <- data_baldoyle[ -c(1:3)]
 #Grouping the data
-data_swords <- data.frame(t(colSums(Filter(is.numeric, data_swords))))
+data_baldoyle <- data.frame(t(colSums(Filter(is.numeric, data_baldoyle))))
 #Adding column Area so I can identify
-data_swords$location <- "Swords"
+data_baldoyle$location <- "Baldoyle"
 
 #Same for Carrickmines
-data_carrickmines <- merged %>% filter(str_detect(EDNAME, "Foxrock-Carrickmines"))
+data_sandyford <- merged %>% filter(str_detect(EDNAME, "Dundrum-Sandyford"))
 #Drop the first three columns
-data_carrickmines <- data_carrickmines[ -c(1:3)]
+data_sandyford <- data_sandyford[ -c(1:3)]
 #Grouping the data
-data_carrickmines <- data.frame(t(colSums(Filter(is.numeric, data_carrickmines))))
+data_sandyford <- data.frame(t(colSums(Filter(is.numeric, data_sandyford))))
 #Adding column Area so I can identify
-data_carrickmines$location <- "Carrickmines"
+data_sandyford$location <- "Sandyford"
 
 ######## Featuring engineering for each of the dataframes ########
-data_swords$T0_4Total <- sum(data_swords$T1_1AGE0T, data_swords$T1_1AGE1T, data_swords$T1_1AGE2T, data_swords$T1_1AGE3T, data_swords$T1_1AGE4T)
+data_baldoyle$T0_4Total <- sum(data_baldoyle$T1_1AGE0T, data_baldoyle$T1_1AGE1T, data_baldoyle$T1_1AGE2T, data_baldoyle$T1_1AGE3T, data_baldoyle$T1_1AGE4T)
 
-data-
+data_baldoyle$T5_9Total <- sum(data_baldoyle$T1_1AGE5T, data_baldoyle$T1_1AGE6T, data_baldoyle$T1_1AGE7T, data_baldoyle$T1_1AGE8T, data_baldoyle$T1_1AGE9T)
 
-data_carrickmines$T0_4Total <- sum(data_carrickmines$T1_1AGE0T, data_carrickmines$T1_1AGE1T, data_carrickmines$T1_1AGE2T, data_carrickmines$T1_1AGE3T, data_carrickmines$T1_1AGE4T)
+data_baldoyle$T10_14Total <- sum(data_baldoyle$T1_1AGE10T, data_baldoyle$T1_1AGE11T, data_baldoyle$T1_1AGE12T, data_baldoyle$T1_1AGE13T, data_baldoyle$T1_1AGE14T)
 
-######## Combining dataframes ######## 
-data <- rbind(data_carrickmines, data_swords)
+data_baldoyle$T15_19Total <- sum(data_baldoyle$T1_1AGE15T, data_baldoyle$T1_1AGE16T, data_baldoyle$T1_1AGE17T, data_baldoyle$T1_1AGE18T, data_baldoyle$T1_1AGE19T)
 
-write.csv(data, file ="outbound.")
+data_sandyford$T0_4Total <- sum(data_sandyford$T1_1AGE0T, data_sandyford$T1_1AGE1T, data_sandyford$T1_1AGE2T, data_sandyford$T1_1AGE3T, data_sandyford$T1_1AGE4T)
+
+data_sandyford$T5_9Total <- sum(data_sandyford$T5_1AGE5T, data_sandyford$T1_1AGE6T, data_sandyford$T1_1AGE7T, data_sandyford$T1_1AGE8T, data_sandyford$T1_1AGE9T)
+
+data_sandyford$T10_14Total <- sum(data_sandyford$T5_1AGE10T, data_sandyford$T1_1AGE11T, data_sandyford$T1_1AGE12T, data_sandyford$T1_1AGE13T, data_sandyford$T1_1AGE14T)
+
+data_sandyford$T15_19Total <- sum(data_sandyford$T5_1AGE15T, data_sandyford$T1_1AGE16T, data_sandyford$T1_1AGE17T, data_sandyford$T1_1AGE18T, data_sandyford$T1_1AGE19T)
+
+######## Combining dataframes######## 
+data <- rbind(data_baldoyle, data_sandyford)
 help(write.csv)
 
-######## Bit of featuring engineering to combine some age sets in bins of 5 years ######## 
-data$T0_4Total <- sum(data$T1_1AGE0T, data$T1_1AGE1T, data$T1_1AGE2T, data$T1_1AGE3T, data$T1_1AGE4T)
+######## Loading in fixed data ######## 
 
-data$T0_4Total
+data_real <- read.csv("outbound.csv")
 
-data$T0_4Total <- data %>% mutate(sumrow = data$T1_1AGE0T, data$T1_1AGE1T, data$T1_1AGE2T, data$T1_1AGE3T, data$T1_1AGE4T)
+######## Histogram of age distribution ######## 
+Age <- data_real[,c(806:810)]
+Age <- data_real[,c(91:104, 806:810)]
+
+Age <- Age[, c(15:19, 1:14)]
+
+#Two historgrams for each location
+histBaldoyle <- subset(Age, location == "Baldoyle")
+histSandyford <- subset(Age, location == "Sandyford")
+
+#Changing them into more user friendly histograms
+histBaldoyle <- histBaldoyle %>% gather(Age_Bracket, value, X0_4, X5_9, X10_14, X15_19, X20_24, X25_29, X30_34, X35_39, X40_44, X45_49, X50_54, X55_59, X60_64, X65_69, X70_74, X75_79, X80_84, X85.)
+
+histSandyford <- histSandyford %>% gather(Age_Bracket, value, X0_4, X5_9, X10_14, X15_19, X20_24, X25_29, X30_34, X35_39, X40_44, X45_49, X50_54, X55_59, X60_64, X65_69, X70_74, X75_79, X80_84, X85.)
+
+
+ggplot(histBaldoyle, aes(x = Age_Bracket, y = value)) + geom_col(fill = "#2fc7da", colour = "black") + ggtitle("Age distribution of Baldoyle") + ylab("Count") + xlab ("Age Bracket") + theme_classic()
+  
+  
+ggplot(histSandyford, aes(x = Age_Bracket, y = value)) + geom_col(fill = "#fcba15", colour = "black") + ggtitle("Age distribution of Sandyford") + ylab("Count") + xlab ("Age Bracket") + theme_classic()
+
 
